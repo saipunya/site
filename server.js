b8c956b561f5cced70c45836dedd4963ef1a254f
saipunya <sumetchoorat@gmail.com>
2025-02-20 22:17:06 +0700

    update20022568

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { publicDecrypt } = require('crypto');
+
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -7,8 +7,14 @@ const ejs = require('ejs');
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'))
 app.get('/',(req,res) => {
     res.render('index')
+})
+app.get('/product/:fruit',(req,res) => {
+    let  fruit = req.params.fruit
+    res.render('product', {data: fruit})
+    
 })
 app.listen(port,()=>{
     console.log('server is listening on port', port);
